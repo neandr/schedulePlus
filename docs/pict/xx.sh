71ac998b11b8df7cfbd6d12d7a0c067cf8ffcaaf
neandr <gnendr@we.de>
2022-07-18 23:40:32 +0200

    update icon&pict

---
+++ docs/pict/xx.sh
@@ -0,0 +1,12 @@
+rm actualDayplan_1.png
+rm dayplanMenus.png
+rm edit_schedulePlus.ini.png
+rm edit_set2.png
+rm flagDE.png
+rm flagUS.png
+rm getGeoPositionG.png
+rm getGeoPositionR.png
+rm jobdef_0.png
+rm MainMenu.png
+rm Tagesplan_Dialoge_0.png
+rm Tagesplan_Dialoge.png
